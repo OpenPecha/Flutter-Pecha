c1a4fd0559023242cbff320b6fc39e2e932c8fd8
TenzDelek <tibetdelek@gmail.com>
2026-08-12 09:41:25 +0530

    group join fix

--- lib/features/group_profile/presentation/providers/group_profile_providers.dart
+++ lib/features/group_profile/presentation/providers/group_profile_providers.dart
@@ -143,7 +143,15 @@ class GroupFollowNotifier extends StateNotifier<GroupFollowState> {
 
   void _invalidateConnectProviders() {
     _ref.invalidate(myGroupsProvider);
-    _ref.invalidate(discoverGroupsProvider);
+  }
+
+  void _refreshDiscoverGroupsIfLoaded() {
+    if (!_ref.exists(discoverGroupsProvider)) return;
+
+    final discoverState = _ref.read(discoverGroupsProvider);
+    if (!discoverState.hasLoaded) return;
+
+    _ref.read(discoverGroupsProvider.notifier).refresh();
   }
 
   void _addPendingJoinedGroup(GroupProfile group) {
@@ -298,6 +306,7 @@ class GroupFollowNotifier extends StateNotifier<GroupFollowState> {
         if (connectGroup != null) {
           _removePendingJoinedGroup(connectGroup.id);
           _markPendingUnjoined(connectGroup.id);
+          _refreshDiscoverGroupsIfLoaded();
         }
         return true;
       },
